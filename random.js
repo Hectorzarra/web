9bd4af71b5128a73d5983cf5de0a4488c24dd465
Hectorzarra <zarrabalrojas@gmail.com>
2024-05-19 15:56:09 -0700

    Add files via upload

---
+++ random.js
@@ -0,0 +1,8 @@
+var respuesta = window.confirm("Hola te quieres casar conmmigo?");
+
+if (respuesta == true) {
+
+	document.write("Te voy hacer chiquillos a lo pendejo mija 🤤🤤🤤🤤🤤");
+} else{
+	document.write("Votate alv estan mejor las de 1500$ ");
+}
